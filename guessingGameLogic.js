be839d12aa605998c85ffa2598dd72bf75d2235d
oddCompost <richomas@hotmail.com>
2022-05-06 11:25:50 +0100

    first push

--- guessingGameLogic.js
+++ guessingGameLogic.js
@@ -1,2 +1,49 @@
 
-console.log('Hello World');
+/************************************************************************
+ *                                                                      *
+ **                      Guessing Game - Amenity42                     **
+ *                                                                      *
+ ************************************************************************/
+
+
+//* ----------------------------| Setup |----------------------------
+
+
+
+class User{
+
+      constructor(Name){
+
+            this. name = Name;
+            this. score = 0;
+            this. highScore;
+
+      }
+
+}
+
+class System{
+
+      constructor(){
+
+
+      }
+
+      static randomNum(){
+
+            let randomNo = Math.round(Math.random() * 6);
+
+            return randomNo;
+
+      }
+
+}
+
+//todo Do this with front end but for now...
+const newUser = new User('Richard');
+
+//* --------------------------| Game Logic |--------------------------
+
+console.log(System.randomNum());
+
+
